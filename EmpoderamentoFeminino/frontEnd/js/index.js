092ccaad99056268915b97fa1bbb0ffbd7b5667e
Welisson Caetano <w.da.silva.leano@avanade.com>
2022-04-26 11:11:17 -0300

    Adeguações finais do FrontEnd e adição do README com orientações para manipular o projeto.

--- EmpoderamentoFeminino/frontEnd/js/index.js
+++ EmpoderamentoFeminino/frontEnd/js/index.js
@@ -1,10 +1,9 @@
 function init() {
-    // console.log("inicio")
     getEmpoderamentos()
-    
+    tableClose = true
 }
 var dados = null
-
+var tableClose = null
 function getEmpoderamentos() {
     var requestOptions = {
         method: 'GET',
@@ -12,23 +11,23 @@ function getEmpoderamentos() {
             "Accept": "application/json",
             'Access-Control-Allow-Origin': '*'
         }
-      };
-     
-      let myRequest = new Request("https://localhost:44362/api/EmpoderamentoFemininos", requestOptions)
+    };
 
-      fetch(myRequest).then((response) =>response.json())
-      .then((data) => {
-        dados = data
-        careegarCarrossel()
-      })
-      .catch(error => {
-          return error;
-      });
-    
+    let myRequest = new Request("https://localhost:44362/api/EmpoderamentoFemininos", requestOptions)
+
+    fetch(myRequest).then((response) => response.json())
+        .then((data) => {
+            dados = data
+            careegarCarrossel()
+        })
+        .catch(error => {
+            return error;
+        });
+
 }
 
 function editar(id) {
-    var selecionado = dados.filter( x =>{
+    var selecionado = dados.filter(x => {
         return x.id == id
     })
     console.log(selecionado)
@@ -37,62 +36,120 @@ function editar(id) {
     $("#site").val(selecionado[0].site)
     $("#url").val(selecionado[0].url)
     $("#descricao").val(selecionado[0].descricao)
-   // $("#collapseExample").Headers()
-
+    colapse()
+    abrir(true)
 }
 
 function deletar(id) {
-    console.log(id)
-}
-function salvar() {
     var myHeaders = new Headers();
-myHeaders.append("content-type", "application/json");
+    myHeaders.append("content-type", "application/json");
+    var url = "https://localhost:44362/api/EmpoderamentoFemininos/" + id
 
-var raw = JSON.stringify({
-  "id": $("#idhiden").val(),
-  "nome":  $("#name").val(),
-  "descricao":  $("#site").val(),
-  "site":  $("#url").val(),
-  "url": $("#descricao").val()
-  
-});
+    var requestOptions = {
+        method: 'DELETE',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
 
-var requestOptions = {
-  method: 'PUT',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
+    fetch(url, requestOptions)
+        .then(response => response.text())
+        .then(result =>{ 
+            dados = dados.filter(function(item) {
+                return item.id !== id
+            })
+            careegarTable()
+        })
+        .catch(error => console.log('error', error));
+}
+function salvar(id) {
+    var myHeaders = new Headers();
+    myHeaders.append("content-type", "application/json");
+    var cod = parseInt(id)
+    var url = "https://localhost:44362/api/EmpoderamentoFemininos";
+    var method = 'POST'
+    if (cod > 0) {
+        url = url + "/" + cod
+        method = 'PUT'
+    }
 
-fetch("https://localhost:44362/api/EmpoderamentoFemininos/7", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+    var raw = JSON.stringify({
+        "id": cod,
+        "nome": $("#name").val(),
+        "descricao": $("#descricao").val(),
+        "site": $("#site").val(),
+        "url": $("#url").val()
+
+    });
+
+    var requestOptions = {
+        method: method,
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch(url, requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            if (!cod)
+                dados.push(result)
+            getEmpoderamentos()
+            careegarTable()
+        })
+        .catch(error => console.log('error', error));
 }
 
 
-function careegarCarrossel(){
+function careegarCarrossel() {
     $("#carrossel").empty()
-    var buuton = ' <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>'
+    var buuton = ' <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 0"></button>'
     var htm = ''
     for (let i = 0; i < dados.length; i++) {
-        buuton += '<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="'+(i+1)+'" aria-label="Slide '+(i+1)+'"></button>'
-        if(i == 0)
-        htm += '<div class="carousel-item active"><img src="../assets/background.png" class="d-block w-100" alt="..."><div class="carousel-caption d-none d-md-block"><h1>Nome: '+dados[i].nome+'</h1><h5>Clique no link para o Youtube:<a href="'+dados[i].url+'" ><i class="bi bi-youtube mg-l color tam"></i></a> </h5><p>'+dados[i].descricao+' <a href="'+dados[i].site+'" > <h2 class="SiteColor">Site Oficial</h2></a>. </p></div></div>'
+        if(i < (dados.length - 1))
+            buuton += '<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="' + (i + 1) + '" aria-label="Slide ' + (i + 1) + '"></button>'
+        if (i == 0)
+            htm += '<div class="carousel-item active"><img src="../assets/background.png" class="d-block w-100" alt="..."><div class="carousel-caption d-none d-md-block"><h1>Nome: ' + dados[i].nome + '</h1><h5>Clique no link para o Youtube:<a href="' + dados[i].url + '" ><i class="bi bi-youtube mg-l color tam"></i></a> </h5><p>' + dados[i].descricao + ' <a href="' + dados[i].site + '" > <h2 class="SiteColor">Site Oficial</h2></a>. </p></div></div>'
         else
-        htm += '<div class="carousel-item "><img src="../assets/background.png" class="d-block w-100" alt="..."><div class="carousel-caption d-none d-md-block"><h1>Nome: '+dados[i].nome+'</h1><h5>Clique no link para o Youtube:<a href="'+dados[i].url+'" ><i class="bi bi-youtube mg-l color tam"></i></a> </h5><p>'+dados[i].descricao+' <a href="'+dados[i].site+'" > <h2 class="SiteColor">Site Oficial</h2></a></p></div></div>'
-    
+            htm += '<div class="carousel-item "><img src="../assets/background.png" class="d-block w-100" alt="..."><div class="carousel-caption d-none d-md-block"><h1>Nome: ' + dados[i].nome + '</h1><h5>Clique no link para o Youtube:<a href="' + dados[i].url + '" ><i class="bi bi-youtube mg-l color tam"></i></a> </h5><p>' + dados[i].descricao + ' <a href="' + dados[i].site + '" > <h2 class="SiteColor">Site Oficial</h2></a></p></div></div>'
+
     }
     $(".carousel-indicators").html(buuton)
     $("#carrossel").html(htm)
 }
 
 
-function careegarTable(){
+function careegarTable() {
     $("#bodytable").empty()
     for (let i = 0; i < dados.length; i++) {
         $("#bodytable").append(
-            '<tr><th scope="row">'+dados[i].id+'</th><td>'+dados[i].nome+'</td><td><button type="button" onclick="editar('+dados[i].id+')" class="btn btn-sm btn-outline-primary"><i class="bi bi-pen-fill"></i></button><button type="button" onclick="deletar('+dados[i].id+')" class="btn btn-sm btn-outline-primary  mg-l" ><i class="bi bi-trash-fill"></i></button></td></tr>'
+            '<tr><th scope="row">' + dados[i].id + '</th><td>' + dados[i].nome + '</td><td><button type="button" onclick="editar(' + dados[i].id + ')" class="btn btn-sm btn-outline-primary"><i class="bi bi-pen-fill"></i></button><button type="button" onclick="deletar(' + dados[i].id + ')" class="btn btn-sm btn-outline-primary  mg-l" ><i class="bi bi-trash-fill"></i></button></td></tr>'
         )
     }
+    limpar()
+}
+
+function colapse() {
+    var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+    var collapseList = collapseElementList.map(function (collapseEl) {
+        return new bootstrap.Collapse(collapseEl)
+    })
+}
+
+function limpar() {
+    $("#idhiden").val("0")
+    $(".limpar").val("")
+}
+
+function form() {
+    if(!tableClose)
+        colapse()
+}
+
+function abrir(open) {
+    tableClose = open;
+}
+
+function reset() {
+    limpar()
+    form()
 }
--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Projeto Dio-Tech-Day
 Repositório para envio de projeto sobre empoderamento feminino Tech Day
+
+# Organização
+O projeto  começa na pasta Dio-Tech-Day nela têm a pasta EmpoderamentoFeminino
+que dentro fica divido em pasta o frontEnd e o BackEnd
+
+    # frontEnd:
+    Solução com uma unica pagina feita em JavaScript 
+    .Jquery, Bootstrap e FecthAPi
+
+    # backEnd:
+    Solução de uma Api Rest com .Net Core 5 persistindo dados com MySql e disponibilização da Swagger da Api
+
+# Tutorial para montar ambiente Local
+
+    Pré-Requisito: 
+        .Microsoft Visual Studio com Dot Net Core 5
+        .MySql instalado justamente com um Gereciador de banco de Dados Ex: WorkBenchMySql
+        .NPM instalado Globalmente
+        .Navegador de Internet Instalado Ex: Google Ghrommer
+        .Visual Studio Code instalado
+        .Git e GitBash instalados
+        .Conhecimento de Git
+
+    1º No GitBash clona o projeto que esta no Github para uma pasta local de sua escolha no seu computador
+    2º Para rodar a Api no Backend, com o Microsoft Visual Studio abrir o projeto que fica
+        ...\Dio-Tech-Day\EmpoderamentoFeminino\backEnd\EmpoderamentoFemenino.sln
+        Alterar a StringConection no aquivo appsettings.json dentro do projeto com os dado de user e password do seu banco 
+        No menu, clicar na aba Build e na opção Build Solution em seguida Rodar a APi clicando no botão PLAY no Microsoft Visual Studio
+
+        Visualizar o sistema abrir uma aba no navegador com uma url LocalHost da Swaagger da Api
+
+    3º Abrir a pasta do FrontEnd com o Visual Studio Code
+        ...\Dio-Tech-Day\EmpoderamentoFeminino\frontEnd
+        Abrir a linha de comando no Visual Studio Code e rodar o Comando npm install
+        Com a Api rodando no Microsoft Visual Studio, na pasta \frontEnd clicar no arquivo index.html para abrir no navegador de internet
+
+    4º No navegador é possivel ver a interface FrontEnd com o usuario comunicando com a Api.
+
+
+# Descrição do FrontEnd
+  Página única com um Carrossel de Empoderamento Feminino e abaixo um botão para um modal
+  que contêm um CRUD.
+    
+# Feito por Elenice Aparecida Silva
